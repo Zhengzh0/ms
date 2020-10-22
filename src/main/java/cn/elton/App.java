5647163de43948e2afe9f5127677b63c5fc8df29
zhengzh0 <297896587@qq.com>
2020-10-22 22:47:06 +0800

    隐藏编译器文件

--- src/main/java/cn/elton/App.java
+++ src/main/java/cn/elton/App.java
@@ -1,12 +1,8 @@
 package cn.elton;
 
-import cn.elton.dao.UserDOMapper;
-import cn.elton.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,19 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("cn.elton.dao")
 public class App
 {
-    @Autowired
-    private UserDOMapper userDOMapper;
-
-    @RequestMapping("/")
-    public String home() {
-
-        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
-        if (userDO == null) {
-            return "用户对象不存在";
-        } else {
-            return userDO.getName();
-        }
-    }
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
